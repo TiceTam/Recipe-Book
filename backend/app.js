39db69fb61592c5cc9741c949046610736aca4dd
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-10-30 13:46:34 -0400

    Potential Digital Ocean fix.

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 3001;
 
 
+app.use(express.static(path.join(__dirname + "/frontend/build")));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');

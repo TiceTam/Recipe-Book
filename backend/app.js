45fbc01834c23c3ca0cd8b42c008afbf5384406d
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-10-30 14:23:05 -0400

    Potential Digital OCean fix.

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,19 @@ const app = express()
 const port = process.env.PORT || 3001;
 
 
-app.use(express.static(path.join(__dirname + "../frontend/build")));
+
+
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
-  });
+});
   
-  
+
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    console.log(path.join(__dirname, "../frontend/build"));
   });

0c5797c1c9688079262a1e9086d1a1fc4d205736
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-10-30 13:27:09 -0400

    Potential Digital Ocean fix.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 3001;
 
 
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.send('Welcome to Express');
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });

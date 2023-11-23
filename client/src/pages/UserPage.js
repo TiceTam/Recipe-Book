d1edc4e0e16e99a2c0e2dfd91c462f85c95fa4d8
2OldForThis2 <winniee1999@gmail.com>
2023-11-23 14:37:02 -0500

    get liked recipe and unlike function

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -3,8 +3,86 @@ import {Link} from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import React, {useEffect, useState} from 'react';
+import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
 
 function UserPage(){
+
+    const [likedRecipe, setLike] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
+    const [instructions, setInstructions] = useState([]);
+    const [openModal, setOpenModal] = useState();
+
+    const getLikes = async () => {
+
+        const URL = "http://localhost:3001/api/loadlikes";
+        var userID = localStorage.getItem("usernameID");
+        const body = JSON.stringify({userID: userID});
+
+        try{
+            const response = await fetch(URL, {
+                method: 'POST',
+                body: body,
+                headers: {
+                	'Content-Type': 'application/json'
+                },
+            });
+            const data = await response.json();
+            //console.log(data);
+            setLike(data.recipes); 
+        } catch(error){
+            console.log(error);
+        }
+    }
+
+    const getIngredients = (ingredientList) => {
+        setIngredients(ingredientList);
+    }
+
+    const getInstructions = (instructionList) => {
+        setInstructions(instructionList);
+    }
+
+    useEffect(() => {
+        getLikes();
+    }, []);
+
+    const onDislike = async (e, id) =>{
+        e.stopPropagation();
+
+        var userID = localStorage.getItem("usernameID");
+        console.log(userID);
+        var recipeID = id;
+
+        const URL = "http://localhost:3001/api/deletelikes";
+        const body = JSON.stringify({userID: userID, recipeID: recipeID});
+
+        try{
+            await fetch(URL, {
+                method: "POST",
+                body: body,
+                headers: {
+                	'Content-Type': 'application/json'
+                },
+            }).then(
+                async(response) => {
+                    var warn = document.getElementById("errorMessage");
+
+                    if(response.status === 200){
+                        const json = await response.json();
+                        console.log(json);
+                        warn.innerHTML = "You successfully dislike this recipe :)";
+                        console.log("Successfully deleted like");
+                        getLikes();
+                    }
+                }
+            )
+        } catch (error){
+            console.log(error);
+        };
+    }
+
     return (
         <div>
             <Navbar expand="lg" className="navbar" sticky='top'>
@@ -29,7 +107,7 @@ function UserPage(){
 
             <div className='lrTextWrap'>
                 <div id="textSearch">
-                    <p className='lrText'>Liked Recipes
+                    <div className='lrText'>Liked Recipes
                         <Container className='mt-2'>
                             <Row>
                                 <Col sm={4}>
@@ -45,10 +123,58 @@ function UserPage(){
                                 </Col>
                             </Row>
                         </Container>
-                    </p>
+                        <p id="errorMessage"></p>
+                    </div>
                 </div>
                 
             </div>
+
+            <Container className='lrCon'>
+                <Row xs={2} md={3} lg={4} className="g-4">
+                    {likedRecipe.map((like) => (
+                        <Col className='lrCol'>
+                        <Card className='flex-fill'>
+                            <Card.Img variant="top" src={like.image} alt={like.recipeName}/>
+                            <Card.Body>
+                                <Card.Title>{like.recipeName}</Card.Title>
+                                <Button variant="warning" onClick={() => {setOpenModal(like._id); getIngredients(like.ingredients); getInstructions(like.instructions);}}>View Recipe</Button>
+                                <Button className="btn btn-block btn-primary" onClick={(event) => onDislike(event, like._id)}>
+                                    Dislike
+                                </Button>        
+                                    <Modal
+                                        show={openModal === like._id}
+                                        size="lg"
+                                        aria-labelledby="contained-modal-title-vcenter"
+                                        centered>
+
+                                        <Modal.Header>
+                                            <Modal.Title id="contained-modal-title-vcenter">INGREDIENTS AND INSTRUCTIONS</Modal.Title>
+                                        </Modal.Header>
+
+                                        <Modal.Body>
+                                            <p className="ingredient">Ingredients:</p>
+                                            {ingredients.map((ingredient) => (
+                                                <p>-{ingredient.ingredient}</p>
+                                            ))}
+
+                                            <p className="instruction">Instructions:</p>
+                                            {instructions.map((instruction) => (
+                                                <p>-{instruction.instruction}</p>
+                                            ))}
+                                        </Modal.Body>
+
+                                        <Modal.Footer>
+                                            <Button onClick={() => setOpenModal(false)}>Close</Button>
+                                        </Modal.Footer>
+                                    </Modal>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+
+
         </div>
     );
 }
